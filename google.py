d7f6ed724ee5625aaa0bee70f3ab2436f5ec5496
이주노 <124761676+juno-bara@users.noreply.github.com>
2023-09-17 23:07:24 +0900

    plus

--- google.py
+++ google.py
@@ -2,8 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+
+
 driver = webdriver.Chrome()
 driver.get("https://www.fsb.or.kr/mobbusmagequar_0100.act")
+elem = driver.find_element(By.ID,"BANK_NAME")
+elem.send_keys("바로")
+elem.send_keys(Keys.RETURN)
+driver.find_element(By.ID,"busiSearch").click()
+
 # assert "Python" in driver.title
 # elem = driver.find_element(By.NAME, "q")
 # elem.clear()
